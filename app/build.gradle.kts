322e669cdae98d892feb462d3d27eccd4b9930e1
Smileheart <smileheart0708@163.com>
2025-08-02 18:03:41 +0800

    添加网络请求依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,8 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging.interceptor)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
